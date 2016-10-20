1ab4cda586b65f04f9afd968ad6a28c36060ae84
TnSolutions <tnsolutions@Mac-mini-di-TnSolutions.local>
2016-10-20 12:57:05 +0200

    Finished

--- model1-solution/app.js
+++ model1-solution/app.js
@@ -1,38 +1,47 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('LunchCheck', [])
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
 
-.controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.state = "normal";
+        $scope.txtinput = "";
+        $scope.message = "";
 
+        $scope.setFocus = function() {
+            $scope.state = "initial";
+            $scope.message = "";
+        }
 
-function LunchCheckController ($scope) {
-  $scope.txtinput = "";
-  $scope.message="Message ";
-
-  $scope.checkInput = function () {
-    $scope.message="Checking.. " + $scope.txtinput;
-    checkInputData($scope.txtinput);
-  }
-
-  function checkInputData(string) {
-  }
-
-
-  $scope.displayNumeric = function () {
-    var totalNameValue = calculatNumericForString($scope.name);
-    $scope.totalValue = totalNameValue;
-  };
-
-  function calculatNumericForString(string) {
-    var totalStringValue = 0;
-    for (var i = 0; i < string.length; i++) {
-      totalStringValue += string.charCodeAt(i);
+        $scope.checkInput = function() {
+            if (!$scope.txtinput) {
+                $scope.message = "Please enter data first";
+                $scope.state = "error";
+                return;
+            }
+            var total = countDishes($scope.txtinput);
+            if (total <= 3) {
+                $scope.message = "Enjoy!";
+                $scope.state = "info";
+                return;
+            }
+            if (total > 3) {
+                $scope.message = "Too much!";
+                $scope.state = "info";
+                return;
+            }
+        }
+        function countDishes(stringToSplit) {
+            var arrayOfStrings = stringToSplit.split(',');
+            var arrayLength = arrayOfStrings.length;
+            //check array for Empty element
+            for (var i = 0; i < arrayOfStrings.length; i++) {
+                if (!(arrayOfStrings[i].trim()))
+                    arrayLength--;
+            }
+            return arrayLength;
+        }
     }
-    return totalStringValue;
-  }
-}
-
-
 })();
